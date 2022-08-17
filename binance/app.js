97d34cec6467a29538d93649277cf36de82cd180
meta-dapp <asizesoft@gmail.com>
2022-08-17 18:05:22 +0200

    Actualización 2 bot de binance

--- binance/app.js
+++ binance/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const Storage = require('node-storage')
+const moment = require('moment')
 const { log, logColor, colors } = require('./utils/logger')
 const client = require('./services/binance')
 
@@ -12,7 +13,13 @@ const store = new Storage(`./data/${MARKET}.json`)
 const sleep = (timeMs) => new Promise(resolve => setTimeout(resolve, timeMs))
 
 async function _balances() {
+    await client.useServerTime()
     return await client.balance()
+}
+
+function elapsedTime() {
+    const diff = Date.now() - store.get('start_time')
+    return moment.utc(diff).format('HH:mm:ss')
 }
 
 function _newPriceReset(_market, balance, price) {
@@ -48,14 +55,23 @@ function _logProfits(price) {
     logColor(isGainerProfit == 1 ?
         colors.green : isGainerProfit == 2 ?
             colors.red : colors.gray,
-        `Global Profits: ${parseFloat(store.get('profits')).toFixed(3)} ${MARKET2}`)
+        `Global Profits (Incl. fees): ${parseFloat(store.get('profits')).toFixed(4)} ${MARKET2}`)
+    log(`Global Fees: ${parseFloat(store.get('fees')).toFixed(4)} ${MARKET2}`)
 
     const m1Balance = parseFloat(store.get(`${MARKET1.toLowerCase()}_balance`))
     const m2Balance = parseFloat(store.get(`${MARKET2.toLowerCase()}_balance`))
 
     const initialBalance = parseFloat(store.get(`initial_${MARKET2.toLowerCase()}_balance`))
     logColor(colors.gray,
-        `Balance: ${m1Balance} ${MARKET1}, ${m2Balance.toFixed(2)} ${MARKET2}, Current: ${parseFloat(m1Balance * price + m2Balance)} ${MARKET2}, Initial: ${initialBalance.toFixed(2)} ${MARKET2}`)
+        `Balance: ${m1Balance} ${MARKET1}, ${m2Balance.toFixed(2)} ${MARKET2}`)
+    logColor(colors.gray,
+        `Current: ${parseFloat(m1Balance * price + m2Balance)} ${MARKET2}, Initial: ${initialBalance.toFixed(2)} ${MARKET2}`)
+}
+
+async function getFees({ commission, commissionAsset }) {
+    const market = `${commissionAsset}USDT`
+    const price = parseFloat((await client.prices(market))[market])
+    return price * commission
 }
 
 async function _buy(price, amount) {
@@ -69,7 +85,9 @@ async function _buy(price, amount) {
             sell_price: price + factor,
             sold_price: 0,
             status: 'pending',
-            profit: 0
+            profit: 0,
+            buy_fee: 0,
+            sell_fee: 0
         }
 
         log(`
@@ -83,6 +101,8 @@ async function _buy(price, amount) {
         if (res && res.status === 'FILLED') {
             order.status = 'bought'
             order.id = res.orderId
+            order.buy_fee = parseFloat((await getFees(res.fills[0])))
+            store.put('fees', parseFloat(store.get('fees')) + order.buy_fee)
             order.buy_price = parseFloat(res.fills[0].price)
 
             orders.push(order)
@@ -131,8 +151,12 @@ async function _sell(price) {
                         if (order.id == toSold[j].id) {
                             toSold[j].profit = (parseFloat(toSold[j].amount) * _price)
                                 - (parseFloat(toSold[j].amount) * parseFloat(toSold[j].buy_price))
+
+                            toSold[j].profit -= order.sell_fee + order.buy_fee
+                            toSold[j].sell_fee = parseFloat((await getFees(res.fills[0])))
                             toSold[j].status = 'sold'
                             orders[i] = toSold[j]
+                            store.put('fees', parseFloat(store.get('fees')) + orders[i].sell_fee)
                         }
                     }
                 }
@@ -165,6 +189,8 @@ async function broadcast() {
                 const marketPrice = mPrice
 
                 console.clear()
+
+                log(`Running Time: ${elapsedTime()}`)
                 log('===========================================================')
                 _logProfits(marketPrice)
                 log('===========================================================')
@@ -192,8 +218,16 @@ async function broadcast() {
                 }
 
                 const orders = store.get('orders')
-                if (orders.length > 0)
-                    console.log(orders[orders.length - 1])
+                if (orders.length > 0) {
+                    const bOrder = orders[orders.length - 1]
+                    console.log()
+                    log('Last buy order')
+                    console.log('==========================')
+                    log(`Buy price: ${bOrder.buy_price}`)
+                    log(`Sell price: ${bOrder.sell_price}`)
+                    logColor(colors.green, `Expected profit: ${parseFloat((bOrder.amount * bOrder.sell_price - bOrder.amount * bOrder.buy_price) - bOrder.buy_fee).toFixed(2)}`)
+                    console.log('==========================')
+                }
             }
         } catch (err) { }
         await sleep(process.env.SLEEP_TIME)
@@ -202,10 +236,13 @@ async function broadcast() {
 
 async function init() {
     if (process.argv[5] !== 'resume') {
+        const startTime = Date.now()
+        store.put('start_time', startTime)
         const price = await client.prices(MARKET)
         store.put('start_price', parseFloat(price[MARKET]))
         store.put('orders', [])
         store.put('profits', 0)
+        store.put('fees', 0)
         const balances = await _balances()
         store.put(`${MARKET1.toLowerCase()}_balance`, parseFloat(balances[MARKET1].available))
         store.put(`${MARKET2.toLowerCase()}_balance`, parseFloat(balances[MARKET2].available))
--- binance/services/binance.js
+++ binance/services/binance.js
@@ -1,7 +1,8 @@
 const Binance = require('node-binance-api')
 const client = new Binance().options({
     APIKEY: process.env.APIKEY,
-    APISECRET: process.env.SECRET
+    APISECRET: process.env.SECRET,
+    useServerTime: true
 })
 
 module.exports = client
